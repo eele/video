65dcf9e746c58c1d6f3a7cbfb5be5b63be2f7a00
eselectron <2727262253@qq.com>
2018-05-28 19:52:07 +0800

    Modify something

--- src/test/java/a/A.java
+++ src/test/java/a/A.java
@@ -7,9 +7,9 @@ public class A {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-//		String path2 = Thread.currentThread().getContextClassLoader().getResource("\\").getPath();  
-//		System.out.println("path2 = " + path2);  
-		
+//		String path2 = Thread.currentThread().getContextClassLoader().getResource("\\").getPath();
+//		System.out.println("path2 = " + path2);
+
 		JaxWsDynamicClientFactory clientFactory = JaxWsDynamicClientFactory.newInstance();
 		Client dynamicClient = clientFactory.createClient("http://localhost:8088/service/uploadInfo?wsdl");
 		dynamicClient.invoke("addVID", "aa");
